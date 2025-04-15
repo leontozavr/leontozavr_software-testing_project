f54567cc1c0c9df8e7913aacf09c4c9c47e17999
leontozavr <theflaffily@gmail.com>
2025-04-15 18:15:28 +0300

    Задание №4: Потренироваться использовать сравнение объектов (доработка) Горбунов Л.А. @leontozavr

--- sandbox/src/main/java/ru/ltz/general/Triangle.java
+++ sandbox/src/main/java/ru/ltz/general/Triangle.java
@@ -1,18 +1,20 @@
 package ru.ltz.general;
 
+import java.util.Objects;
+
 public record Triangle (double side1, double side2, double side3) {
 
     public Triangle {
         if (side1 < 0 || side2 < 0 || side3 < 0) {
             throw new IllegalArgumentException("Сторона треугольника не может быть отрицательной");
         }
-        if ( (side1 + side2) < side3 || (side1 + side3) < side2 || (side2 + side3) < side1 ) {
+        if ((side1 + side2) < side3 || (side1 + side3) < side2 || (side2 + side3) < side1) {
             throw new IllegalArgumentException("Сумма 2-х любых сторон треугольника должна быть не меньше третьей стороны");
         }
     }
 
     public double calculatePerimeter() {
-        return this.side1 + this.side2 +this.side3;
+        return this.side1 + this.side2 + this.side3;
     }
 
     public double calculateArea() {
@@ -22,16 +24,23 @@ public record Triangle (double side1, double side2, double side3) {
 
     @Override
     public boolean equals(Object o) {
+        if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Triangle triangle = (Triangle) o;
+        return (Double.compare(this.side1, triangle.side1) == 0 && Double.compare(this.side2, triangle.side2) == 0 && Double.compare(this.side3, triangle.side3) == 0) ||
 
-        return ((triangle.side1 == this.side1) && (triangle.side2 == this.side2) && (triangle.side3 == this.side3)) ||
-                ((triangle.side1 == this.side2) && (triangle.side2 == this.side3) && (triangle.side3 == this.side1)) ||
-                ((triangle.side1 == this.side3) && (triangle.side2 == this.side1) && (triangle.side3 == this.side2));
+                (Double.compare(this.side1, triangle.side1) == 0 && Double.compare(this.side2, triangle.side3) == 0 && Double.compare(this.side3, triangle.side2) == 0) ||
+                (Double.compare(this.side1, triangle.side3) == 0 && Double.compare(this.side2, triangle.side2) == 0 && Double.compare(this.side3, triangle.side1) == 0) ||
+                (Double.compare(this.side3, triangle.side1) == 0 && Double.compare(this.side2, triangle.side2) == 0 && Double.compare(this.side1, triangle.side3) == 0) ||
+                (Double.compare(this.side1, triangle.side2) == 0 && Double.compare(this.side2, triangle.side1) == 0 && Double.compare(this.side3, triangle.side3) == 0) ||
+                (Double.compare(this.side2, triangle.side1) == 0 && Double.compare(this.side1, triangle.side2) == 0 && Double.compare(this.side3, triangle.side3) == 0) ||
+
+                (Double.compare(this.side1, triangle.side2) == 0 && Double.compare(this.side2, triangle.side3) == 0 && Double.compare(this.side3, triangle.side1) == 0) ||
+                (Double.compare(this.side1, triangle.side3) == 0 && Double.compare(this.side2, triangle.side1) == 0 && Double.compare(this.side3, triangle.side2) == 0);
     }
 
     @Override
     public int hashCode() {
-        return 1;
+        return Objects.hash(side1, side2, side3);
     }
 }
